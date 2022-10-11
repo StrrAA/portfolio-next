5b4ac7e7de2cc6dcc7a9f5de78d1c4c06e095b27
STRRAAA <stracovskymichal@gmail.com>
2022-10-11 21:57:55 +0200

    bug fix17TryCatch

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -15,7 +15,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const pageInfo: PageInfo = await sanityClient.fetch(query);
-
-  res.status(200).json({ pageInfo });
+  try {
+	const pageInfo: PageInfo = await sanityClient.fetch(query);
+	
+	  res.status(200).json({ pageInfo });
+} catch (error) {
+	console.log("error fetching page info", error);
+}
 }
