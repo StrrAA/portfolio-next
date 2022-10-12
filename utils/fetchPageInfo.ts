17a0937b568231f43b3432af74adb0e99e4ba49f
STRRAAA <stracovskymichal@gmail.com>
2022-10-12 02:48:20 +0200

    test user agent

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,7 +2,14 @@ import { PageInfo } from "../typings";
 
 export const fetchPageInfo = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'User-Agent': '*',
+      },
+    }
   );
 
   const data = await res.json();
